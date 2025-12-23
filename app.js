4b4926e184e75687bc10d55543589ae28da51176
yosef <the.wood.8080@gmail.com>
2025-12-23 11:16:38 +0200

    work in progress

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require('express');
 const app=express();
 const productRouter=require('./api/v1/routes/product');
@@ -5,7 +6,11 @@ const orderRouter=require('./api/v1/routes/order')
 const morgan=require('morgan');
 const ipFilter=require('./api/v1/middlewares/ipFilter');
 app.use(morgan('dev'));
-
+const mongoUser=process.env.MONGO_USER
+const mongoPass=process.env.MONGO_PASS
+const mongoserver=process.env.MONGO_SERVER
+const mongoConstr=`mongodb+srv://${mongoUser}:${mongoPass}@${mongoserver}/?appName=Cluster0`
+console.log(mongoConstr);
 app.use(ipFilter);
 
 
